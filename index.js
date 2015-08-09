989ac94f9b835675c14da080b13c4918ef4469d0
hibernius <korova.cow@gmail.com>
2015-08-10 00:06:33 +0100

    Extend WP functionality to full download-config-createDB-corefiles install

--- index.js
+++ index.js
@@ -3,11 +3,74 @@
 var prompt = require('prompt');
 
 var args = process.argv.slice(2);
-var config = {
-	name: args[0] || 'springbox-project',
-	path: '~/htdocs/'
+
+var projectname = args[0] || 'springbox-project'
+
+var options = {
+	name: {
+		default: projectname,
+		short: projectname.replace('-', ''),
+		title: projectname.replace('-', ' ')
+	},
+	wp: {}
 };
 
-console.log(config);
+options.wp.plugins = [
+	"advanced-text-widget",
+	"breadcrumb-navxt",
+	"bulk-page-creator",
+	"better-wp-security",
+	"cms-tree-page-view",
+	"force-regenerate-thumbnails",
+	"imsanity",
+	"widget-logic",
+	"wp-password-generator",
+	"wordpress-seo"
+];
 
-var wp = require('./lib/wordpress.js')(config);
+options.wp.themes = [
+	"https://github.com/bigspring/monolith/archive/master.zip"
+];
+
+var promptSchema = {
+	properties: {
+		db_username: {
+			description: 'Database username',
+			pattern: /^[a-zA-Z0-9\-]+$/,
+			message: 'Name must be only letters, numbers or dashes',
+			required: true
+		},
+		db_password: {
+			description: 'Database password',
+			hidden: true
+		},
+		wp_username: {
+			description: 'Wordpress username',
+			pattern: /^[a-zA-Z0-9]+$/,
+			message: 'Name must be only letters and numbers',
+			required: true
+		},
+		wp_password: {
+			description: 'Wordpress Password',
+			required: true
+		},
+		wp_prefix: {
+			description: 'Wordpress table prefix (excluding underscore)',
+			required: true
+		}
+	}
+};
+
+prompt.start();
+prompt.get(
+	promptSchema,
+	function (err, res) {
+		if (err) {
+			console.log('SPRINGBOX > An error occurred: ');
+			console.log(err);
+			return false;
+		}
+		console.log('SPRINGBOX > Installing WP...');
+		var wp = require('./lib/wordpress.js')(res, options);
+	}
+);
