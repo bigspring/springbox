ff5353aa4514ecc95031df75a8ebbc53eee42c25
hibernius <korova.cow@gmail.com>
2015-08-15 14:53:55 +0100

    Move prompt into separate module.

--- index.js
+++ index.js
@@ -1,64 +1,27 @@
 #! /usr/bin/env node
 
 var prompt = require('prompt');
-
-var args = process.argv.slice(2);
-var projectname = args[0] || 'springbox-project'
+var promptSchema = require('./lib/prompt.schema.js');
 
 var options = {
 	wp: {
-		theme: "https://github.com/bigspring/monolith/archive/master.zip"
+		theme: 'https://github.com/bigspring/monolith/archive/master.zip'
 	}
 };
 
 options.wp.plugins = [
-	"advanced-text-widget",
-	"better-wp-security",
-	"breadcrumb-navxt",
-	"bulk-page-creator",
-	"custom-post-type-ui",
-	"cms-tree-page-view",
-	"force-regenerate-thumbnails",
-	"imsanity",
-	"widget-logic",
-	"wp-password-generator",
-	"wordpress-seo"
+	'advanced-text-widget',
+	'better-wp-security',
+	'breadcrumb-navxt',
+	'bulk-page-creator',
+	'cms-tree-page-view',
+	'custom-post-type-ui',
+	'force-regenerate-thumbnails',
+	'imsanity',
+	'widget-logic',
+	'wp-password-generator',
+	'wordpress-seo'
 ];
-
-var promptSchema = {
-	properties: {
-		projectname: {
-			description: 'Project name',
-			pattern: /^[a-zA-Z0-9\-]+$/,
-			message: 'Project name must be only letters, numbers or dashes',
-			required: true
-		},
-		db_username: {
-			description: 'Database username',
-			pattern: /^[a-zA-Z0-9\-]+$/,
-			message: 'Username must be only letters, numbers or dashes',
-			required: true
-		},
-		db_password: {
-			description: 'Database password',
-			hidden: true
-		},
-		wp_username: {
-			description: 'Wordpress username',
-			pattern: /^[a-zA-Z0-9\_]+$/,
-			message: 'Username must be only letters and numbers',
-			required: true
-		},
-		wp_password: {
-			description: 'Wordpress password',
-			required: true
-		},
-		wp_prefix: {
-			description: 'Wordpress table prefix (excluding underscore)',
-			required: true
-		}
-	}
-};
 
 prompt.start();
 prompt.get(
